c585ba0fd812546ba7535456e2d164444b824203
Barrett Quan <21barrett@gmail.com>
2017-03-09 11:41:28 -0800

    finalized formatting and layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-console.log('hello from main.js')
+console.log('welcome visitor! ✌️️')
 
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
+})
+
+
+//smooth scroll animation
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+//random color picker for fun!
+$('.mystery').on('click', (evt) => {
+
+  var safeColors = ['00','33','66','99','cc','ff'];
+  var rand = function() {
+    return Math.floor(Math.random()*6);
+  };
+  var randomColor = function() {
+    var r = safeColors[rand()];
+    var g = safeColors[rand()];
+    var b = safeColors[rand()];
+    return "#"+r+g+b;
+  };
+ $('.greeting').css('color',randomColor())
 })
