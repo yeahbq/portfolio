8d5e0dcdd627b40b62f08e6a698473995c91df23
Barrett Quan <21barrett@gmail.com>
2017-05-23 19:36:45 -0700

    centered icons and banner

--- js/main.js
+++ js/main.js
@@ -20,20 +20,3 @@ $(function() {
   });
 });
 
-
-//random color picker for fun!
-$('.mystery').on('click', (evt) => {
-
-  var safeColors = ['00','33','66','99','cc','ff'];
-  var rand = function() {
-    return Math.floor(Math.random()*6);
-  };
-  var randomColor = function() {
-    var r = safeColors[rand()];
-    var g = safeColors[rand()];
-    var b = safeColors[rand()];
-    return "#"+r+g+b;
-  };
- $('.greeting').css('color',randomColor())
-})
-
